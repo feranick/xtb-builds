2ce4ebb00235b911023ac1ecfa11740220aa9740
Nicola Ferralis <feranick@hotmail.com>
2025-03-14 10:40:32 -0400

    bug fixes

--- Ubuntu24.04/openblas/open_blas.sh
+++ Ubuntu24.04/openblas/open_blas.sh
@@ -1,12 +1,11 @@
 #!/bin/sh 
 
 sudo apt -y update
-sudo apt -i install  libblas-dev liblapack-dev libopenblas-dev 
+sudo apt -y install  libblas-dev liblapack-dev libopenblas-dev 
 
 install_folder=$HOME"/Software/xtb/xtb-bin"
 
 git clone https://github.com/grimme-lab/xtb.git
 cd xtb
-#git fetch origin pull/1215/head 
 meson setup build --prefix=$install_folder --buildtype=release -Dlapack=openblas --wipe
 ninja -C build install
